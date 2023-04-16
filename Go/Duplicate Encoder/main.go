7bc8281a11e26d3b6f29cfc14b7d426fe46ed20b
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-16 17:18:47 -0400

    [Go] DuplicateEncode - Adding comments

--- Go/Duplicate Encoder/main.go
+++ Go/Duplicate Encoder/main.go
@@ -8,23 +8,30 @@ import (
 
 func main() {
 	word := os.Args[1]
-	
+
 	fmt.Println(DuplicateEncode(word))
 }
 
 func DuplicateEncode(word string) string {
+	// transform word to lower case
 	wordToLower := strings.ToLower(word)
-	characters := make(map [rune]string)
-	
+	// Use a map to check if we already have a char
+	// withint the word
+	characters := make(map[rune]string)
+
 	for _, char := range wordToLower {
+		// If we have a character it means
+		// we should replace it by ")"
 		if _, ok := characters[char]; ok {
 			characters[char] = ")"
 			continue
-		} 
+		}
 
+		// By default we replace by "("
 		characters[char] = "("
 	}
 
+	// We build our string using our map
 	str := ""
 	for _, char := range wordToLower {
 		str += characters[char]
