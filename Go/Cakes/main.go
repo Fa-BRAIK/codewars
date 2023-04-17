9c7409203f05328591eb82b2caebd3d7c1a6db0a
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-17 17:52:10 -0400

    [Go] Cakes

---
+++ Go/Cakes/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	recipe := map[string]int{"flour": 500, "sugar": 200, "eggs": 1}
+	available := map[string]int{"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+
+	fmt.Println(Cakes(recipe, available))
+}
+
+func Cakes(recipe, available map[string]int) int {
+	mina := 0
+
+	for i, qty := range recipe {
+		if qtya, ok := available[i]; ok {
+			if qtya < qty {
+				return 0
+			}
+
+			if amount := qtya / qty; amount < mina || mina == 0 {
+				mina = amount
+			}
+
+			continue
+		}
+
+		return 0
+	}
+
+	return mina
+}
