100e3eec5fc5e6479761b76fac2ddd188763aec5
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-17 18:47:34 -0400

    [Go] ToWeirdCase - second solution

--- Go/ToWeirdCase/main.go
+++ Go/ToWeirdCase/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 func main() {
 	fmt.Println(toWeirdCase("Weird string case"))
+	fmt.Println(toWeirdCaseRunes("Weird string case"))
 }
 
 func toWeirdCase(str string) (wstr string) {
@@ -26,4 +28,23 @@ func toWeirdCase(str string) (wstr string) {
 	}
 	
 	return
+}
+
+func toWeirdCaseRunes(str string) string {
+	toUpper := true
+	var result []rune
+	for _, c := range str {
+		if (c == 32) {
+			result = append(result, c)
+			toUpper = true
+		} else if (toUpper) {
+			result = append(result, unicode.ToUpper(c))
+			toUpper = false
+		} else {
+			result = append(result, unicode.ToLower(c))
+			toUpper = true
+		}
+	}
+	
+	return string(result)
 }
