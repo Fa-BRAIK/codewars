703b19bb92b2ee7416e31fba9c0fe9c1b1ec0de0
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-17 18:28:00 -0400

    [Go] HumanReadableTime - updating solution

--- Go/HumanReadableTime/main.go
+++ Go/HumanReadableTime/main.go
@@ -5,13 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println(HumanReadableTime(0))
+	fmt.Println(HumanReadableTime(3956))
 }
 
-func HumanReadableTime(seconds int) string {
-	h := int(seconds / 60 / 60)
-	m := int((seconds - h * 60 * 60) / 60)
-	s := seconds - (h * 60 * 60) - (m * 60)
+func HumanReadableTime(s int) string {
+	m,s := s / 60, s % 60
+    h,m := m / 60, m % 60
 
-	return fmt.Sprintf("%02d", h) + ":" + fmt.Sprintf("%02d", m) + ":" + fmt.Sprintf("%02d", s)
+	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
 }
