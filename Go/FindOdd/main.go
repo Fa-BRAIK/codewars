50e9e33289d601aff204ca7e87f7c208886a12fb
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-18 18:04:49 -0400

    [Go] FindOdd

---
+++ Go/FindOdd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5}
+
+	fmt.Println(FindOdd(nums))
+}
+
+func FindOdd(seq []int) (odd int) {
+	nums := map [int]int{}
+
+	for _, num := range seq {
+		nums[num]++
+	}
+
+	for num, v := range nums {
+		if v % 2 != 0 {
+			odd = num; break
+		}
+	}
+
+	return 
+}
