d0b834552b6c5943d18125d405826f0404c332a0
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-18 19:14:33 -0400

    [Go] ProductFib adding better solution

--- Go/ProductFib/main.go
+++ Go/ProductFib/main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println(ProductFib(5895))
+	fmt.Println(ProductFibLinear(5895))
 }
 
+// O(n^2)
 func ProductFib(prod uint64) (res [3]uint64) {
 	fs := map[uint64]uint64{
 		0: 0,
@@ -43,4 +45,20 @@ func ProductFib(prod uint64) (res [3]uint64) {
 	}
 
 	return
+}
+
+func ProductFibLinear(prod uint64) [3]uint64 {
+	f1 := uint64(0)
+	f2 := uint64(1)
+
+	for f1 * f1 < prod {
+		f1, f2 = f2, f1 + f2
+	}
+
+	success := uint64(0)
+	if prod == f1 * f2 {
+		success = 1
+	}
+
+	return [3]uint64{f1, f2, success}
 }
