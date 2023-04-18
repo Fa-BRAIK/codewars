a4ce1bd54b31923f97cbe4c1974d6f3da71939c7
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-18 18:09:12 -0400

    [Go] FindOdd - adding better soluton

--- Go/FindOdd/main.go
+++ Go/FindOdd/main.go
@@ -6,6 +6,7 @@ func main() {
 	nums := []int{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5}
 
 	fmt.Println(FindOdd(nums))
+	fmt.Println(FindOddXor(nums))
 }
 
 func FindOdd(seq []int) (odd int) {
@@ -22,4 +23,12 @@ func FindOdd(seq []int) (odd int) {
 	}
 
 	return 
+}
+
+func FindOddXor(seq []int) (odd int) {
+	for _, n := range seq {
+		odd ^= n
+	}
+
+	return
 }
