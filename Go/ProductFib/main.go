a9badfc63ba6f74420cb4422d266cea2e1146a01
Fa-BRAIK <faroukbraik17@gmail.com>
2023-04-18 19:09:23 -0400

    [Go] ProductFib

---
+++ Go/ProductFib/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(ProductFib(5895))
+}
+
+func ProductFib(prod uint64) (res [3]uint64) {
+	fs := map[uint64]uint64{
+		0: 0,
+		1: 1,
+	}
+	var n uint64 = 2
+
+	for {
+		i := uint64(2)
+		for i <= n + 1 {
+			fs[i] = fs[i - 1] + fs[i - 2] 
+			i++
+		}
+
+		f1 := fs[i - 2]
+		f2 := fs[i - 1]
+
+		mul := f1 * f2
+
+		if mul == prod {
+			res[0] = f1
+			res[1] = f2
+			res[2] = 1
+			break
+		}
+
+		if mul > prod {
+			res[0] = f1
+			res[1] = f2
+			res[2] = 0
+			break
+		}
+
+		n++
+	}
+
+	return
+}
